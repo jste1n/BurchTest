092fc4b9b1f3fe51b0982ee8de38266994fdcf77
jsteinwender-tgm <steinwenderjan@hotmail.com>
2017-04-03 01:57:37 +0200

    comments added

--- AdditionalTests.py
+++ AdditionalTests.py
@@ -1,9 +1,15 @@
 import unittest
 from bruch.Bruch import *
+
+
 class AdditionalTests(unittest.TestCase):
+    """
+    5 weitere test
+    steinwender
+    """
     def setUp(self):
-        self.b = Bruch(10,20)
-        self.b2 = Bruch(50,25)
+        self.b = Bruch(10, 20)
+        self.b2 = Bruch(50, 25)
         pass
 
     def tearDown(self):
@@ -11,20 +17,34 @@ class AdditionalTests(unittest.TestCase):
         pass
 
     def testVal(self):
-        assert(float(self.b)==0.5)
+        """
+        calculates the fraction to a real number
+        """
+        assert (float(self.b) == 0.5)
 
     def testInvert(self):
+        """
+        inverts the number
+        """
         b3 = ~self.b
-        assert(float(b3)==2)
+        assert (float(b3) == 2)
 
     def testNegate(self):
+        """
+        negation of a number
+        """
         b3 = Bruch(-65, 93)
-        assert(-b3 == Bruch(65, 93))
+        assert (-b3 == Bruch(65, 93))
 
     def testSmaller(self):
-        assert(self.b<self.b2)
+        """
+        tests if first number is smaller than second one
+        """
+        assert (self.b < self.b2)
 
     def testToString(self):
-        b3=Bruch(2,3)
-        assert("(2/3)"==str(b3))
-
+        """
+        converts Bruch to a string
+        """
+        b3 = Bruch(2, 3)
+        assert ("(2/3)" == str(b3))
